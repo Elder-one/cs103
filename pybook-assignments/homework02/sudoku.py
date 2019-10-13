0a41306ec9d0c4f751c89a79b7309e725327ac22
Elder-one <aleksan672@gmail.com>
2019-10-13 03:29:19 +0300

    Реализованы ф-ции find_empty_positions() и find_possible_values() и solve()

--- pybook-assignments/homework02/sudoku.py
+++ pybook-assignments/homework02/sudoku.py
@@ -37,7 +37,7 @@ def group(values: List[str], n: int) -> List[List[str]]:
 
         for _1 in range(n):
 
-            result[-1].append(List[i])
+            result[-1].append(values[i])
             i += 1
 
     return result
@@ -111,7 +111,13 @@ def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    pass
+    n = len(grid)
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == '.':
+                return (i, j)
+
+    return (-1, -1)
 
 
 def find_possible_values(grid: List[List[str]], pos: Tuple[int, int]) -> Set[str]:
@@ -125,7 +131,15 @@ def find_possible_values(grid: List[List[str]], pos: Tuple[int, int]) -> Set[str
     >>> values == {'2', '5', '9'}
     True
     """
-    pass
+    result = set()
+
+    for ch in '123456789':
+        if ch not in get_row(grid, pos):
+            if ch not in get_col(grid, pos):
+                if ch not in get_block(grid, pos):
+                    result.add(ch)
+
+    return result
 
 
 def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
@@ -141,7 +155,30 @@ def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    empty_pos = find_empty_positions(grid)
+
+    if empty_pos == (-1, -1):
+        return grid
+
+    values = find_possible_values(grid, empty_pos)
+
+    if len(values) == 0:
+        return
+
+    row, col = empty_pos
+
+    for value in values:
+
+        grid[row][col] = value
+
+        result = solve(grid)
+
+        if result:
+            return result
+
+    grid[row][col] = '.'
+    return #комментарий, чтобы закоммитить эту ф-цию
+
 
 
 def check_solution(solution: List[List[str]]) -> bool:
